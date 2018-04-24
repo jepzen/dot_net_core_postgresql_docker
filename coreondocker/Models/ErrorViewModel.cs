aa588cafbf0e9ebc64bac1d63eb1f18340d0ef96
Jeppe b. Svendsen <jeppesvendsen@gmail.com>
2018-04-24 10:55:45 +0100

    Add project files.

---
+++ coreondocker/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace coreondocker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
